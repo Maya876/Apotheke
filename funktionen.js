ae213995e0a1e37cd2bf882e99dadd99d9d64fe9
Maya876 <maya.tyrrell1907@gmail.com>
2026-07-21 14:50:01 +0200

    toogle

---
+++ funktionen.js
@@ -0,0 +1,13 @@
+function toggleSidebar() {
+  var sidebar = document.getElementById("mySidebar");
+  var content = document.getElementById("mainContent");
+  
+  sidebar.classList.toggle("active");
+  
+  // Verschiebt den Inhalt um 250px nach rechts, wenn Sidebar offen
+  if (sidebar.classList.contains("active")) {
+    content.style.marginLeft = "250px";
+  } else {
+    content.style.marginLeft = "0";
+  }
+}
